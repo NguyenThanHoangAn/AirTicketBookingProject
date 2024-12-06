7b16a2ddbc9e48322e0172ffa09b8832123e163f
Vinh Kha <vinhkha7171@gmail.com>
2024-12-06 14:12:41 +0700

    second code kha

--- doan/src/index.js
+++ doan/src/index.js
@@ -4,15 +4,24 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import AdminFlightForm from './components/AdminFlightForm';
+import FlightManagement from './components/Flightmanagement';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Routes>
+    <Route path="/" element={<App />}/>
+    <Route path="/adminflightform" element={<AdminFlightForm />}/>
+    <Route path="/flightmanagement" element={<FlightManagement />}/>
+    </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
